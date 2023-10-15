ba6167b4a30b8f7ef6ceec1ad4cc6d032b661e49
Axel Martinez Sanjuan <axelmartinez212001@gmail.com>
2023-10-15 11:06:57 -0600

    Configuracion conexion a BD

---
+++ system-course-backend/src/main/java/com/system/ams/SystemCourseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.system.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemCourseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemCourseBackendApplication.class, args);
+	}
+
+}
---
+++ system-course-backend/src/test/java/com/system/ams/SystemCourseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemCourseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
